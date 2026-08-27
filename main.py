391c4eb961964c3c997b20c9847249eae10b4d35
Poogle <liug06@mit.edu>
2026-08-26 20:13:26 -0400

    domain

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://localhost:5175",
         "http://localhost:5176",
+        "https://nmt-sdt0.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
